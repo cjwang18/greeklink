49014b59b4d1db40f2428b0a331247aa2b58c021
CJ Wang <chunjiwa@usc.edu>
2013-07-03 21:32:50 -0700

    woops, forgot to rename a file; css works again

--- testdrive/protected/views/layouts/landing.php
+++ testdrive/protected/views/layouts/landing.php
@@ -8,7 +8,7 @@
 
 		<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/bootstrap/bootstrap.css" />
 		<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/bootstrap/bootstrap-responsive.css" />
-		<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/home-style.css" />
+		<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/landing-style.css" />
 
 		<title><?php echo CHtml::encode($this->pageTitle); ?></title>
 	</head>

270ee4cc9a42d0bab9d3a42f135d99096fa10d2b
CJ Wang <chunjiwa@usc.edu>
2013-07-05 10:29:00 -0700

    edited signup confirmation message

--- testdrive/protected/views/site/pages/signupConfirmation.php
+++ testdrive/protected/views/site/pages/signupConfirmation.php
@@ -18,9 +18,12 @@ $this->breadcrumbs=array(
 
 		<p>
 			In order to maintain the exclusivity of GreekLink, 
-			your account will need to be verified by your organization's National.
-			This process may take up to 3 days. Please be patient but be on a lookout
+			your account will need to be verified by your organization's 
+			national headquarters.
+			This process may take up to 3 days. We know you can't wait to connect
+			with your brothers and sisters so be on a lookout
 			for an email confirming your account's activation!
+			Thanks for signing up with GreekLink!
 		</p>
 
 	</div>

5431efe54b84c6c62af3ac335a05276c8b45fb1b
Luis Guan <luis.guan.wu@gmail.com>
2013-07-05 15:55:30 -0700

    Styled the sign up confirmation page

--- testdrive/protected/views/site/pages/signupConfirmation.php
+++ testdrive/protected/views/site/pages/signupConfirmation.php
@@ -12,19 +12,22 @@ $this->breadcrumbs=array(
 
 	<div class="form-signin">
 
-		<h1>You've signed up!</h1>
+		<div class="confirmationHeader">You've signed up!</div>
 
-		<h2>But wait:</h2>
+			<div class="confirmationContents">
+				<div class="confirmationSubHeader">But wait:</div>
 
-		<p>
-			In order to maintain the exclusivity of GreekLink, 
-			your account will need to be verified by your organization's 
-			national headquarters.
-			This process may take up to 3 days. We know you can't wait to connect
-			with your brothers and sisters so be on a lookout
-			for an email confirming your account's activation!
-			Thanks for signing up with GreekLink!
-		</p>
+				<p>
+					In order to maintain the exclusivity of GreekLink, 
+					your account will need to be verified by your organization's 
+					national headquarters.
+					This process may take up to 3 days. We know you can't wait to connect
+					with your brothers and sisters so be on a lookout
+					for an email confirming your account's activation!
+					Thanks for signing up with GreekLink!
+				</p>
+
+			</div>
 
 	</div>
 </div>
